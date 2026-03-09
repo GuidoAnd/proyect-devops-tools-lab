92cae4c8f46de4003b8a3f0e595dcacf67c9e9ca
Guido <guido.andino@outlook.com>
2026-03-09 15:42:11 +0000

    Try to automatizate k8s.

---
+++ k8s/deploy.sh
@@ -0,0 +1,7 @@
+kubectl apply -f orders-api/namespace.yaml
+kubectl create secret -n orders-api docker-registry regcred \
+    --docker-server=id.dkr.ecr.us-east-1.amazonaws.com/-/test:latest \
+    --docker-username=AWS \
+    --docker-password=$(aws ecr get-login-password --region us-east-1)
+kubectl apply -f orders-api/deployment.yaml
+kubectl apply -f orders-api/service.yaml
